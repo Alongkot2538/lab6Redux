bc5dae101ad77f4ddd329c937dd418d2a8c0cc22
alongkot2538 <alongkot628@gmail.com>
2020-03-05 05:33:02 +0700

    lab6

---
+++ react-bear/src/redux/store.js
@@ -0,0 +1,38 @@
+
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from 'redux-logger'
+const initialForm = {
+    name: '',
+    weight: 0,
+    img: ''
+}
+const fromReducer = (data = initialForm, action) => {
+    switch (action.type) {
+        case 'CHANGE_NAME': return { ...data, name: action.name };
+        case 'CHANGE_WEIGHT': return { ...data, weight: action.weight };
+        case 'CHANGE_IMG': return { ...data, img: action.img };
+        default: return data;
+    }
+
+}
+
+const bearReducer = (bears = [], action) => {
+    switch (action.type) {
+        case 'GET_BEARS': return action.bears;
+        case 'ADD_BEAR': return [...bears, action.bear];
+        case 'DELETE_BEAR': return bears.filter(bear => +bear.id!== +action.id)
+        case 'UPDATE_BEAR': return bears.map(bear => {
+            if (+bear.id === +action.id)
+                return action.bear;
+            else return bear;
+        })
+    }
+    return bears;
+}
+
+const reducers = combineReducers({
+    bear: bearReducer,
+    form: fromReducer
+})
+
+export const store = createStore(reducers, applyMiddleware(logger));
